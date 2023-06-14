1b0c210ce3ec43e136fb841b6a8404ff4ecc5112
Vincent Delitz <vincent.delitz@tum.de>
2023-06-14 20:13:42 +0200

    remove authentication url

--- src/routes/corbadoWebhookRoutes.js
+++ src/routes/corbadoWebhookRoutes.js
@@ -5,10 +5,8 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const { AUTHENTICATION_URL: authenticationURL, PROJECT_ID: projectID, API_SECRET: apiSecret, WEBHOOK_USERNAME: webhookUsername, WEBHOOK_PASSWORD: webhookPassword } = process.env;
+const { PROJECT_ID: projectID, API_SECRET: apiSecret, WEBHOOK_USERNAME: webhookUsername, WEBHOOK_PASSWORD: webhookPassword } = process.env;
 const config = new Configuration(projectID, apiSecret);
-config.authenticationURL = authenticationURL;
-
 config.webhookUsername = webhookUsername;
 config.webhookPassword = webhookPassword;
 const corbado = new CorbadoSDK(config);
