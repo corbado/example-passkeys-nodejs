226ff95ded684df4f8a945870021d55a52991bf3
Dopeamin <mohamed.amine.hamdouni@corbado.com>
2024-09-16 13:26:38 +0200

    upgrade into sdk v3

--- src/authController.ts
+++ src/authController.ts
@@ -1,31 +1,37 @@
-import {Request, Response} from 'express';
-import {SDK, Config} from '@corbado/node-sdk';
-import {config as dotenvConfig} from "dotenv";
+import { Request, Response } from "express";
+import { SDK, Config } from "@corbado/node-sdk";
+import { config as dotenvConfig } from "dotenv";
 
-dotenvConfig()
+dotenvConfig();
 
 const projectID = process.env.PROJECT_ID;
 const apiSecret = process.env.API_SECRET;
+const frontendAPI = process.env.FRONTEND_API;
+const backendAPI = process.env.BACKEND_API;
 
-const cboConfig = new Config(projectID!, apiSecret!);
+const cboConfig = new Config(projectID!, apiSecret!, frontendAPI!, backendAPI!);
 const sdk = new SDK(cboConfig);
 
 export function auth(req: Request, res: Response) {
-    res.render('pages/login')
+  res.render("pages/login");
 }
 
 export async function profile(req: Request, res: Response) {
-    const shortSession = req.cookies.cbo_short_session
-    if (!shortSession) {
-        res.redirect("/")
-    }
-    try {
-        const user = await sdk.sessions().getCurrentUser(shortSession)
-        const cboId = user.getID()
-        const email = user.getEmail()
-        res.render('pages/profile', {cboId, email})
-    } catch (e) {
-        console.log(e)
-        res.redirect("/")
-    }
+  const shortSession = req.cookies.cbo_short_session;
+  if (!shortSession) {
+    res.redirect("/");
+  }
+  try {
+    const user = await sdk.sessions().validateToken(shortSession);
+    const cboId = user.userId;
+    const identifiers = await sdk
+      .identifiers()
+      .listByUserIdAndType(cboId, "email");
+    const email = identifiers.identifiers[0].value;
+
+    res.render("pages/profile", { cboId, email });
+  } catch (e) {
+    console.log(e);
+    res.redirect("/");
+  }
 }
